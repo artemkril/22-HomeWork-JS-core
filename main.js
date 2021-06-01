5eeab9c5051050b81ee5003a5f0e876ccf15afe5
artemkril <artemkent9@gmail.com>
2021-06-01 18:49:22 +0300

    HomeWork22 JS Core

---
+++ main.js
@@ -0,0 +1,66 @@
+
+
+$(document).ready(() => {
+
+    // Завдання1
+    // function randomizer(min, max) {
+    //     return min + Math.round(Math.random() * (max - min));
+    // }
+
+    // $('.elem').click(function () {
+    //     // console.log('sdfsdf');
+    //     let h = randomizer(50, 500);
+    //     let w = randomizer(50, 500);
+    //     let top = randomizer(0, 300);
+    //     let right = randomizer(0, 700);
+    //     let r = randomizer(0, 255);
+    //     let g = randomizer(0, 255);
+    //     let b = randomizer(0, 255);
+    //    $(this).css('background', `rgb(${r},${g},${b})`) 
+    //     $('.elem').animate({
+    //        top: top+'px',
+    //        height: h+'px',
+    //        width: w+'px',
+    //        right: right+'px',
+    //     },2000, 'easeOutQuad')    
+    // })
+// Завдання2
+// function randomizer(min, max) {
+//         return min + Math.round(Math.random() * (max - min));
+//     }
+
+// function getAimate(){
+//     let top = randomizer(0, 300);
+//         let right = randomizer(0, 700);
+//         let r = randomizer(0, 255);
+//         let g = randomizer(0, 255);
+//         let b = randomizer(0, 255);
+//         let r2 = randomizer(0, 255);
+//         let g2 = randomizer(0, 255);
+//         let b2 = randomizer(0, 255);
+//         let r3 = randomizer(0, 255);
+//         let g3 = randomizer(0, 255);
+//         let b3 = randomizer(0, 255);
+//     $('.elem').css('background',`rgb(${r},${g},${b})` )
+//     $('.elem').css('border',`8px solid rgb(${r2},${g2},${b2})` )
+//     $('.elem').css('boxShadow',`10px 5px 10px 0 rgb(${r3},${g3},${b3})` )
+//     $('.elem').animate({
+//         top: top+'px',
+//         right: right+'px'
+//     },1000, "easeOutBounce")
+// }
+// getAimate();
+// setInterval(getAimate,1000);
+
+// $('.element').click(function(){
+//     $('.element').animate({
+//             left: '50%',
+//             width: '200px',
+//             height: '200px',
+//         },3000, 'easeOutCubic').animate({
+//                 left: '1%',
+//                 width: '100px',
+//                 height: '100px',
+//             },3000, 'easeOutCubic')
+// }) 
+})
